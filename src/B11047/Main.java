84d87a6adeb453efdb36b666964047b749462314
Hyunah Lee <hyuna030619@naver.com>
2024-08-19 16:55:55 +0900

    update

--- src/B11047/Main.java
+++ src/B11047/Main.java
@@ -17,12 +17,15 @@ public class Main {
         }
 
         for (int i = N - 1; i >= 0; i--) {
+            // 가치가 큰 것부터 for문 돌면서
+            // K를 해당 가치로 나눈 값이 1보다 작으면 넘어가고
             if(K / coins[i] < 1) {
                 continue;
             }
+            // K를 해당 가치로 나누어서 1 이상이면
             else {
-                cnt += K / coins[i];
-                K = K % coins[i];
+                cnt += K / coins[i]; // K를 가치로 나눈 몫을 cnt에 더해주고
+                K = K % coins[i]; // K는 그 나머지로 다시 설정한다.
             }
         }
 
--- src/B9012/Main.java
+++ src/B9012/Main.java
@@ -13,6 +13,7 @@ public class Main {
 
         for (int i = 0; i < T; i++) {
             String inputData = br.readLine();
+            // isVPS가 true면 "YES", false면 "NO" 출력
             if(isVPS(inputData)) {
                 System.out.println("YES");
             } else {
@@ -25,18 +26,24 @@ public class Main {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
+            // 여는 괄호면 스택에 push
             if (c == '(') {
                 stack.push(c);
             }
+            // 닫는 괄호라면
             else if (c == ')') {
+                // 스택이 비어있으면 함께 pop할 여는 괄호가 없으므로 false
                 if (stack.empty()) {
                     return false;
-                } else {
+                }
+                // 그게 아니라면 직전 여는 괄호 pop
+                else {
                     stack.pop();
                 }
             }
         }
 
+        // stack이 비었으면 true, 값이 남았다면 false를 return
         return stack.empty();
     }
 }
