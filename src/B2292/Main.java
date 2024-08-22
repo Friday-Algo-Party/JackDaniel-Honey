0a882ee5aff5605ed99dabbd4e67b5e0156920d0
Hyunah Lee <hyuna030619@naver.com>
2024-08-23 01:24:12 +0900

    update

---
+++ src/B2292/Main.java
@@ -0,0 +1,24 @@
+package B2292;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int beeCnt = 1; // 현재 층
+        int currentMax = 1; // 현재 층의 최대 방 번호
+
+        while (N > currentMax) {
+            currentMax += 6 * beeCnt;
+            beeCnt++;
+        }
+
+        System.out.println(beeCnt);
+    }
+}
