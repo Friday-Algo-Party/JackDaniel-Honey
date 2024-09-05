0a11e1bccec3dc34d2fbeb3c5d719a53862bb975
Hyunah Lee <hyuna030619@naver.com>
2024-09-05 15:17:44 +0900

    #2751 update

---
+++ src/B2751_수정렬하기2/Main.java
@@ -0,0 +1,31 @@
+package B2751_수정렬하기2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            arr.add(Integer.valueOf(br.readLine()));
+        }
+
+        Collections.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int num: arr) {
+            sb.append(num).append('\n');
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
