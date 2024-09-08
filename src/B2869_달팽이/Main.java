0e2f29e7583fd75e3568fed88c871953d173a7e5
Hyunah Lee <hyuna030619@naver.com>
2024-09-09 00:24:22 +0900

    #2869 update

--- src/B2869_달팽이/Main.java
+++ src/B2869_달팽이/Main.java
@@ -13,12 +13,15 @@ public class Main {
         int B = Integer.parseInt(st.nextToken()); // 밤에 미끄러짐
         int V = Integer.parseInt(st.nextToken()); // 나무 막대의 높이
 
-        // 마지막 날은 미끄러지지 않으므로 V - A 만큼 남은 거리를 A - B 로 나눈 후 1을 더해준다.
         int days = (V - B) / (A - B);
-        if ((V - B) % (A - B) != 0) {
-            days++;  // 나머지가 있으면 하루 더 걸림
+        if((V - B) % (A - B) != 0) {
+            days++;
         }
 
         System.out.println(days);
+
+//        System.out.printf("%.0f\n", Math.ceil((double)(V - A) / (A - B)) + 1);
+
+
     }
 }
