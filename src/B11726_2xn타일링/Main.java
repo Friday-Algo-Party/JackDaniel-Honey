49ecefc1c1b4fcd792069f2a6a4ea4777e543a9f
Hyunah Lee <hyuna030619@naver.com>
2024-09-23 19:36:26 +0900

    #11726 주석 추가

--- src/B11726_2xn타일링/Main.java
+++ src/B11726_2xn타일링/Main.java
@@ -9,13 +9,16 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        if (n == 1) {
-            System.out.println(1);
-            return;
-        }
+//        if (n == 1) {
+//            System.out.println(1);
+//            return;
+//        }
 
         // dp 사용
-        int[] tiles = new int[n + 1];
+        int[] tiles = new int[n + 2];
+        // n + 2가 아니라 n + 1로 크기를 지정하면,
+        // n이 1일 때 tiles에 0, 1까지만 넣을 수 있는데
+        // 아래에서 tiles[2] = 2 라고 값을 지정해 버려서 ArrayIndexOutofBoundsException 발생
         tiles[1] = 1;
         tiles[2] = 2;
 
